2d245d106120b8126362bbd18c343285fbb75a3a
Tomas Vondra <tomas@pgaddict.com>
2022-01-20 16:43:01 +0100

    rework scripts for i5
    - restart postgres between queries and drop caches to get uncached
      first run
    
    - run only the 22 queries
    
    - do just explain, not explain analyze

--- i5/run-clang.sh
+++ i5/run-clang.sh
@@ -4,7 +4,7 @@ PATH_OLD=$PATH
 BUILD_DIR=/var/lib/postgresql/builds
 LOG_DIR=/var/lib/postgresql/compiler-test/logs
 
-killall -9 postgres
+killall -q -9 postgres
 sleep 10
 
 for cc in 12 11 10 9; do
@@ -12,8 +12,6 @@ for cc in 12 11 10 9; do
 	for march in "x86-64"; do
 
 		for optim in O0 O1 O2 O3 Os Oz Og Ofast; do
-
-			# for fp in "no-omit-frame-pointer" "omit-frame-pointer"; do
 
 				BUILD="pg-clang-$cc-$march-$optim"
 				mkdir $LOG_DIR/$BUILD
@@ -30,7 +28,7 @@ for cc in 12 11 10 9; do
 
 				popd
 
-				PATH=$BUILD_DIR/$BUILD/bin:$PATH
+				PATH=$BUILD_DIR/$BUILD/bin:$PATH_OLD
 
 				pg_ctl -D /mnt/raid/data-tpch -l $LOG_DIR/$BUILD/pg.log -w start
 
@@ -38,7 +36,7 @@ for cc in 12 11 10 9; do
 
 				pg_config > $LOG_DIR/$BUILD/pg_config.log 2>&1
 
-				dropdb test
+				dropdb --if-exists test
 				createdb test
 
 				psql test < sql/schema.sql > $LOG_DIR/$BUILD/schema.log 2>&1
@@ -73,37 +71,57 @@ for cc in 12 11 10 9; do
 
 				echo "$cc;$march;$optim;analyze;$d" >> load.csv 2>&1
 
+				# run queries with and without JIT enabled
 				for jit in on off; do
 
-					psql test -c "alter system set jit = $jit" > /dev/null 2>&1
+					# create directory for explains and query results
+					mkdir $LOG_DIR/$BUILD/$jit
+					mkdir $LOG_DIR/$BUILD/$jit/explains/
+					mkdir $LOG_DIR/$BUILD/$jit/results/
 
+					# enable/disable the JIT
+					psql test -c "alter system set jit = $jit" > /dev/null 2>&1
 					psql test -c "select pg_reload_conf()" > /dev/null 2>&1
 
-					for q in `ls queries`; do
+					# run just the 22 standard queries, ignore the flattened variants etc.
+					for q in `seq 1 22`; do
 
+						# restart the server before each query, drop caches
+						pg_ctl -D /mnt/raid/data-tpch -l $LOG_DIR/$BUILD/pg.log -w restart
+						sudo ./drop-caches.sh
+
+						# do the plain explain
+						sed 's/EXPLAIN_COMNAND/explain/g' explain/$q.sql | psql test >> $LOG_DIR/$BUILD/$it/explains/$q.log 2>&1
+
+						# calculate hash of the explain, so that we can compare later
+						hp=`md5sum $LOG_DIR/$BUILD/$jit/explains/$q.log | awk '{print $1}'`
+
+						# restart the server again - some of the explains may create objects etc.
+						pg_ctl -D /mnt/raid/data-tpch -l $LOG_DIR/$BUILD/pg.log -w restart
+						sudo ./drop-caches.sh
+
+						# now do three runs for the query
 						for r in `seq 1 3`; do
 
 							s=`psql test -t -A -c "select extract(epoch from now())"`
 
-							psql test < queries/$q >> $LOG_DIR/$BUILD/queries.log 2>&1
+							psql test < queries/$q >> $LOG_DIR/$BUILD/$jit/results/$q.$r.log 2>&1
 
 							t=`psql test -t -A -c "select extract(epoch from now()) - $s"`
 
-							echo "$cc;$march;$optim;$jit;$q;$r;$t" >> queries.csv 2>&1
+							# calculate hash of the result, so that we can compare later
+							hr=`md5sum $LOG_DIR/$BUILD/$jit/results/$q.$r.log | awk '{print $1}'`
+
+							# we assume the plans do not change
+							echo "$cc;$march;$optim;$jit;$q;$r;$t;$hp;$hr" >> queries.csv 2>&1
 
 						done
-
-						echo "===== $q jit=$jit =====" >> $LOG_DIR/$BUILD/explain.log 2>&1
-
-						psql test < explain/$q >> $LOG_DIR/$BUILD/explain.log 2>&1
 
 					done
 
 				done
 
 				pg_ctl -D /mnt/raid/data-tpch -l $LOG_DIR/$BUILD/pg.log -w stop
-
-			# done
 
 		done
 
--- i5/run-gcc-2.sh
+++ i5/run-gcc-2.sh
@@ -4,10 +4,10 @@ PATH_OLD=$PATH
 BUILD_DIR=/var/lib/postgresql/builds
 LOG_DIR=/var/lib/postgresql/compiler-test/logs
 
-killall -9 postgres
+killall -q -9 postgres
 sleep 10
 
-for cc in gcc-10.3.0 gcc-11.2.0 gcc-9.3.0 gcc-4.8.5; do
+for cc in 10.3.0 11.2.0 9.3.0 4.8.5; do
 
 	for march in "x86-64" "native"; do
 
@@ -15,14 +15,12 @@ for cc in gcc-10.3.0 gcc-11.2.0 gcc-9.3.0 gcc-4.8.5; do
 
 			for fp in "no-omit-frame-pointer" "omit-frame-pointer"; do
 
-				BUILD="pg-$cc-$march-$optim-$fp"
+				BUILD="pg-gcc-$cc-$march-$optim-$fp"
 				mkdir $LOG_DIR/$BUILD
 
 				pushd ~/postgres
 
-
-
-				CC=$cc ./configure --prefix=$BUILD_DIR/$BUILD --enable-debug CFLAGS="-march=$march -$optim -f$fp" > $LOG_DIR/$BUILD/configure.log 2>&1
+				CC=gcc-$cc ./configure --prefix=$BUILD_DIR/$BUILD --enable-debug CFLAGS="-march=$march -$optim -f$fp" > $LOG_DIR/$BUILD/configure.log 2>&1
 
 				make -s clean > /dev/null 2>&1
 
@@ -38,7 +36,7 @@ for cc in gcc-10.3.0 gcc-11.2.0 gcc-9.3.0 gcc-4.8.5; do
 
 				pg_config > $LOG_DIR/$BUILD/pg_config.log 2>&1
 
-				dropdb test
+				dropdb --if-exists test
 				createdb test
 
 				psql test < sql/schema.sql > $LOG_DIR/$BUILD/schema.log 2>&1
@@ -47,49 +45,69 @@ for cc in gcc-10.3.0 gcc-11.2.0 gcc-9.3.0 gcc-4.8.5; do
 				psql test < sql/load.sql > $LOG_DIR/$BUILD/load.log 2>&1
 				d=`psql -t -A test -c "select extract(epoch from now()) - $s"`
 
-				echo "$cc;$march;$optim;copy;$d" >> load.csv 2>&1
+				echo "$cc;$march;$optim;$fp;copy;$d" >> load.csv 2>&1
 
 				s=`psql -t -A test -c "select extract(epoch from now())"`
 				psql test < sql/indexes.sql > $LOG_DIR/$BUILD/indexes.log 2>&1
 				d=`psql -t -A test -c "select extract(epoch from now()) - $s"`
 
-				echo "$cc;$march;$optim;indexes;$d" >> load.csv 2>&1
+				echo "$cc;$march;$optim;$fp;indexes;$d" >> load.csv 2>&1
 
 				s=`psql -t -A test -c "select extract(epoch from now())"`
 				psql test < sql/pkey.sql > $LOG_DIR/$BUILD/pkey.log 2>&1
 				d=`psql -t -A test -c "select extract(epoch from now()) - $s"`
 
-				echo "$cc;$march;$optim;pkeys;$d" >> load.csv 2>&1
+				echo "$cc;$march;$optim;$fp;pkeys;$d" >> load.csv 2>&1
 
 				s=`psql -t -A test -c "select extract(epoch from now())"`
 				psql test < sql/fkey.sql > $LOG_DIR/$BUILD/fkey.log 2>&1
 				d=`psql -t -A test -c "select extract(epoch from now()) - $s"`
 
-				echo "$cc;$march;$optim;fkeys;$d" >> load.csv 2>&1
+				echo "$cc;$march;$optim;$fp;fkeys;$d" >> load.csv 2>&1
 
 				s=`psql -t -A test -c "select extract(epoch from now())"`
 				psql test -c "vacuum analyze" > /dev/null 2>&1
 				d=`psql -t -A test -c "select extract(epoch from now()) - $s"`
 
-				echo "$cc;$march;$optim;analyze;$d" >> load.csv 2>&1
+				echo "$cc;$march;$optim;$fp;analyze;$d" >> load.csv 2>&1
 
-				for q in `ls queries`; do
+				# create directory for explains and query results
+				mkdir $LOG_DIR/$BUILD/explains/
+				mkdir $LOG_DIR/$BUILD/results/
 
+				# run just the 22 standard queries, ignore the flattened variants etc.
+				for q in `seq 1 22`; do
+
+					# restart the server before each query, drop caches
+					pg_ctl -D /mnt/raid/data-tpch -l $LOG_DIR/$BUILD/pg.log -w restart
+					sudo ./drop-caches.sh
+
+					# do the plain explain
+					sed 's/EXPLAIN_COMNAND/explain/g' explain/$q.sql | psql test >> $LOG_DIR/$BUILD/explains/$q.log 2>&1
+
+					# calculate hash of the explain, so that we can compare later
+					hp=`md5sum $LOG_DIR/$BUILD/explains/$q.log | awk '{print $1}'`
+
+					# restart the server again - some of the explains may create objects etc.
+					pg_ctl -D /mnt/raid/data-tpch -l $LOG_DIR/$BUILD/pg.log -w restart
+					sudo ./drop-caches.sh
+
+					# now do three runs for the query
 					for r in `seq 1 3`; do
 
 						s=`psql test -t -A -c "select extract(epoch from now())"`
 
-						psql test < queries/$q >> $LOG_DIR/$BUILD/queries.log 2>&1
+						psql test < queries/$q >> $LOG_DIR/$BUILD/results/$q.$r.log 2>&1
 
 						t=`psql test -t -A -c "select extract(epoch from now()) - $s"`
 
-						echo "$cc;$march;$optim;$fp;$q;$r;$t" >> queries.csv 2>&1
+						# calculate hash of the result, so that we can compare later
+						hr=`md5sum $LOG_DIR/$BUILD/results/$q.$r.log | awk '{print $1}'`
+
+						# we assume the plans do not change
+						echo "$cc;$march;$optim;$fp;$q;$r;$t;$hp;$hr" >> queries.csv 2>&1
 
 					done
-
-					echo "===== $q =====" >> $LOG_DIR/$BUILD/explain.log 2>&1
-
-					psql test < explain/$q >> $LOG_DIR/$BUILD/explain.log 2>&1
 
 				done
 
